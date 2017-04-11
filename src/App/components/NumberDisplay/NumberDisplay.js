8195912892b8f8a3ba1d210e17a24b3a55aa6ec3
Chronoes <martentarkin@gmail.com>
2017-04-11 23:08:50 +0300

    Added audio segments for number counting

--- src/App/components/NumberDisplay/NumberDisplay.js
+++ src/App/components/NumberDisplay/NumberDisplay.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import Odometer from 'odometer';
 
+import drumRoll from './drum-roll-loopable.ogg';
+import cymbal from './cymbal.ogg';
+
 import './NumberDisplay.css';
 
 class NumberDisplay extends Component {
-  componentDidMount() {
-    const odometer = this.constructOdometer();
-    odometer.update(this.props.number);
+  componentWillMount() {
+    // Ensure audio is loaded before mounting
+    this.drumRollAudio = new Audio(drumRoll);
+    this.cymbalAudio = new Audio(cymbal);
   }
 
-  constructOdometer() {
+  componentDidMount() {
+    const odometerDuration = 2000;
+    const odometer = this.constructOdometer(odometerDuration);
+    odometer.update(this.props.number);
+    this.drumRollAudio.play();
+    setTimeout(() => {
+      this.cymbalAudio.play();
+      this.drumRollAudio.pause();
+    }, odometerDuration);
+  }
+
+  constructOdometer(duration) {
     return new Odometer({
       el: this.odometerNode,
       value: [...this.props.maximum.toString(10)].map(() => '0').join(''), // 1000 -> 0000
       theme: 'minimal',
+      duration,
     });
   }
 
