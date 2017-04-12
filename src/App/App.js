e05b9b377254ca96e4db7dcd97795756566d4063
Chronoes <martentarkin@gmail.com>
2017-04-12 12:02:06 +0300

    Render regular experience

--- src/App/App.js
+++ src/App/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       minimum: 0,
       maximum: 1000,
       number: 0,
-    }
+    };
   }
 
   generateNumber = (minimum, maximum) => this.setState(oldState => ({
@@ -28,8 +28,80 @@ class App extends Component {
 
   restartGeneration = () => this.setState(oldState => ({ ...oldState, generating: false }));
 
-  render() {
+  renderParameterForm() {
+    const { minimum, maximum } = this.state;
+    return (
+      <main className="col-12 col-sm-8 col-md-6 col-lg-4 flip-out" key="1">
+        <ParameterForm
+          key="0"
+          minimum={minimum}
+          maximum={maximum}
+          onGenerateNumber={this.generateNumber}
+        />
+      </main>
+    );
+  }
+
+  renderRestartButton() {
+    return (
+      <div className="col-12 col-sm-8 col-md-6 col-lg-4">
+        <button
+          className="btn btn-primary mt-2 btn-block"
+          onClick={this.restartGeneration}>
+          Restart
+        </button>
+      </div>
+    );
+  }
+
+  renderAnimatedContent() {
     const { number, generating, minimum, maximum } = this.state;
+    return (
+      <CSSTransitionGroup
+        transitionName="flip-out"
+        transitionEnterTimeout={200}
+        transitionLeaveTimeout={200}>
+        {generating ? (
+          <main className="col-12 flip-out" key="0">
+            <div className="row">
+              <div className="col-12">
+                <NumberDisplay
+                  number={number}
+                  maximum={maximum}
+                  isJohnCena={minimum === 420 && maximum === 1337}
+                />
+              </div>
+              {this.renderRestartButton()}
+            </div>
+          </main>
+        ) : (
+          this.renderParameterForm()
+        )}
+      </CSSTransitionGroup>
+    );
+  }
+
+  renderContent() {
+    const { number, generating } = this.state;
+    return (
+      generating ? (
+        <main className="col-12">
+          <div className="row">
+            <div className="col-12">
+              <h1 className="number-display">{number}</h1>
+            </div>
+            {this.renderRestartButton()}
+          </div>
+        </main>
+      ) : (
+        this.renderParameterForm()
+      )
+    );
+  }
+
+  render() {
+    const { animated } = this.props;
+
     return (
       <div className="app-wrapper">
         <div className="container">
@@ -41,40 +113,7 @@ class App extends Component {
             </div>
           </header>
           <div className="row">
-            <CSSTransitionGroup
-              transitionName="flip-out"
-              transitionEnterTimeout={200}
-              transitionLeaveTimeout={200}>
-              {generating ? (
-                <main className="col-12 flip-out" key="0">
-                  <div className="row">
-                    <div className="col-12">
-                      <NumberDisplay
-                        number={number}
-                        maximum={maximum}
-                        isJohnCena={minimum === 420 && maximum === 1337}
-                      />
-                    </div>
-                    <div className="col-12 col-sm-8 col-md-6 col-lg-4">
-                      <button
-                        className="btn btn-primary mt-2 btn-block"
-                        onClick={this.restartGeneration}>
-                        Restart
-                      </button>
-                    </div>
-                  </div>
-                </main>
-              ) : (
-                <main className="col-12 col-sm-8 col-md-6 col-lg-4 flip-out" key="1">
-                  <ParameterForm
-                    key="0"
-                    minimum={minimum}
-                    maximum={maximum}
-                    onGenerateNumber={this.generateNumber}
-                  />
-                </main>
-              )}
-            </CSSTransitionGroup>
+            {animated ? this.renderAnimatedContent() : this.renderContent()}
           </div>
         </div>
       </div>
